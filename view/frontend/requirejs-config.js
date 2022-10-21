a3fb1c3490d3f15121f206183d8b042b95e689d4
Jean-Michel Batty <jean-michel.batty@extend.com>
2022-10-21 15:33:44 -0500

    initial commit of module

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,12 @@
+var config =
+    {
+        map:
+            {
+                '*':
+                    {
+                        simpleProductWarranty: 'Extend_CustomAPEC/js/product/simple-warranty',
+                        configurableProductWarranty: 'Extend_CustomAPEC/js/product/configurable-warranty',
+                        groupedProductWarranty: 'Extend_CustomAPEC/js/product/grouped-warranty'
+                    }
+            }
+    };
